6da84d897dcc855dbaab5bd12c65d7008a0d7ba7
CaptainEze <110047810+CaptainEze@users.noreply.github.com>
2023-06-23 09:08:37 +0100

    Update _pass.py

--- conf/_pass.py
+++ conf/_pass.py
@@ -1,3 +1,3 @@
 admin = "mte-213-cmmd2"
 guest = "guest_mte_2020/2021"
-repo = "https://github.com/CaptainEze/MTE-group2-analysis_model_application/archive/refs/tags/v0.0.1-beta.zip"
+repo = "https://github.com/CaptainEze/MTE-group2-analysis_model_application/archive/refs/tags/v0.0.2-beta.zip"
